03e274deb50be58d7bd23790d9c421dca9339504
Cheng You Kang Ryan <144519215+ryanz921@users.noreply.github.com>
2025-05-30 18:04:31 +0800

    remove secret from gitignore

---
+++ secrets.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC-ZKL-e0Suzq1Ksfrd-MVIJvWJ6IDmsOg",
+    authDomain: "jj-food-express.firebaseapp.com",
+    projectId: "jj-food-express",
+    storageBucket: "jj-food-express.firebasestorage.app",
+    messagingSenderId: "805781844824",
+    appId: "1:805781844824:web:60835a184f3b72acf42b2d",
+    measurementId: "G-3KDQ0SRGDK"
+};
+
+export const emailjsPublicKey = "X1xSngXceBLYQeFCb";
+export const emailjsServiceId = "service_o101ejy";
+export const emailjsTemplateId = "template_ezlrlss"; 
